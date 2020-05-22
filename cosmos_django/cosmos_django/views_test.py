cea8dc21c18a90f6de1f4d8bb8795ca90be0201d
Darshan Kalola <darshankalola@Darshans-MBP.attlocal.net>
2020-05-22 12:50:20 -0700

    Add landing page skeleton (lmao)

---
+++ cosmos_django/cosmos_django/views_test.py
@@ -0,0 +1,10 @@
+from django.test import Client, TestCase
+from django.urls import reverse
+
+
+class TestViews(TestCase):
+
+    def test_landing_page(self):
+        client = Client()
+        response = client.get(reverse('cosmos_django/landing_page'))
+        self.assertEqual(response.status_code, 200)
