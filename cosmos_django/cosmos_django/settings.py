462eb59f2fe0b3e6c040c263345a5b3f10f60795
Darshan Kalola <darshankalola@Darshans-MBP.attlocal.net>
2020-05-21 19:56:46 -0700

    Switch to postgres database

--- cosmos_django/cosmos_django/settings.py
+++ cosmos_django/cosmos_django/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'cosmos_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cosmos_db',
+        'USER': 'postgres',
+        'PASSWORD': 'gPCiiz=kM;pM&t8',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
