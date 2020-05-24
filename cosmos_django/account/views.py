697ad525fc0bc5bedc922fc9fcf2294830af4bea
Darshan Kalola <darshankalola@Darshans-MBP.attlocal.net>
2020-05-24 12:04:42 -0700

    Replace calls to Account model to get_user_model() method

--- cosmos_django/account/views.py
+++ cosmos_django/account/views.py
@@ -1,7 +1,6 @@
 from django.contrib import auth
+from django.contrib.auth import get_user_model
 from django.shortcuts import render, redirect
-
-from . import models
 
 
 def signin(request):
@@ -33,15 +32,15 @@ def signup(request):
 
         if password == password_check:
             try:
-                models.Account.objects.get(email=email)
+                get_user_model().objects.get(email=email)
                 return render(
                     request,
                     'account/signup.html',
                     {'error': 'Username already exists!'})
-            except models.Account.DoesNotExist:
+            except get_user_model().DoesNotExist:
                 # No existing user exists, create user successfully and bring
                 # them to the home page.
-                user = models.Account.objects.create_user(
+                user = get_user_model().objects.create_user(
                     email=email, password=password)
                 auth.login(request, user)
                 return redirect('clinical/home')
--- cosmos_django/cosmos_django/views.py
+++ cosmos_django/cosmos_django/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 
 
 def landing_page(request):
+    """Load the landing page."""
     if request.user.is_authenticated:
         return redirect('clinical/home')
     return render(request, '../templates/cosmos_django/landing_page.html')
