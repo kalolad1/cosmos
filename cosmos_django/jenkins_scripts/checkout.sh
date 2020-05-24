cd311c14ac3fa63264ad774eea1bddf04a8b0094
Darshan Kalola <darshankalola@Darshans-MBP.attlocal.net>
2020-05-23 20:11:59 -0700

    Edit checkout script.

--- cosmos_django/jenkins_scripts/checkout.sh
+++ cosmos_django/jenkins_scripts/checkout.sh
@@ -2,6 +2,14 @@
 
 ssh jenkins@161.35.102.77 <<EOF
   ls
-  cd cosmos/cosmos_django
+  cd cosmos_cloned/cosmos_django
+  source venv/bin/activate
+  git pull
+  pip install -r requirements.txt
+  python manage.py makemigrations
+  python manage.py migrate
+  python manage.py collectstatic --noinput
+
+  python manage.py test -p *_test.py
   exit
 EOF
