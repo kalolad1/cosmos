578ac3f10fc804f13c1ddb32ea83d0a375484ace
Darshan Kalola <darshankalola@Darshans-MBP.attlocal.net>
2020-05-23 20:21:52 -0700

    Edit Jenkinsfile

--- cosmos_django/jenkins_scripts/stage.sh
+++ cosmos_django/jenkins_scripts/stage.sh
@@ -3,6 +3,6 @@
 ssh jenkins@161.35.102.77 <<EOF
   cd cosmos_cloned/cosmos_django
   source venv/bin/activate
-  venv/bin/python3.6 manage.py runserver 0.0.0.0:8000
+  timeout 5 venv/bin/python3.6 manage.py runserver 0.0.0.0:8000
   exit
 EOF
