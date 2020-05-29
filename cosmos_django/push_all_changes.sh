944d392a681faf99eb4b1964bff5d3073ca61609
Darshan <darshanvkalola@gmail.com>
2020-05-28 20:46:59 -0700

    Add code submit script

---
+++ cosmos_django/push_all_changes.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Script to push all changes to Github.
+# run collectstatic
+python manage.py collectstatic --noinput
+python manage.py makemigrations
+python manage.py migrate
+npm run dev
+pip freeze > requirements.txt
+yapf . --recursive -i
+mypy .
+python manage.py test -p *_test.py
+git add -A
+git commit -m "$1"
+# git push
+
+
+
+
