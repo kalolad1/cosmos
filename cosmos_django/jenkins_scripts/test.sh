8ba8553a441ceb7bc881b303d3dba541e71fb826
Darshan <darshanvkalola@gmail.com>
2020-06-06 22:04:50 -0700

    Remove force reinstallation of deps

--- cosmos_django/jenkins_scripts/test.sh
+++ cosmos_django/jenkins_scripts/test.sh
@@ -4,7 +4,7 @@ ssh jenkins@161.35.102.77 <<EOF
   cd cosmos_cloned/cosmos_django
   source venv/bin/activate
   git pull
-  pip3 install --upgrade --force-reinstall -r requirements.txt
+  pip3 install -r requirements.txt
   venv/bin/python3.6 manage.py makemigrations
   venv/bin/python3.6 manage.py migrate
   venv/bin/python3.6 manage.py collectstatic --noinput
