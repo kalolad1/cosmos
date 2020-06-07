1c7b523d14d880bff54a52bee3a1a045b9d42298
Darshan <darshanvkalola@gmail.com>
2020-06-06 22:08:05 -0700

    Add npm update to deploy script

--- cosmos_django/jenkins_scripts/deploy.sh
+++ cosmos_django/jenkins_scripts/deploy.sh
@@ -4,6 +4,7 @@ ssh cosmos_deploy_user@167.99.154.182 <<EOF
   cd cosmos/cosmos_django
   source venv/bin/activate
   git pull
+  npm update
   pip3 install -r requirements.txt
   venv/bin/python3.6 manage.py makemigrations
   venv/bin/python3.6 manage.py migrate
--- cosmos_django/jenkins_scripts/test.sh
+++ cosmos_django/jenkins_scripts/test.sh
@@ -4,6 +4,7 @@ ssh jenkins@161.35.102.77 <<EOF
   cd cosmos_cloned/cosmos_django
   source venv/bin/activate
   git pull
+  npm update
   pip3 install -r requirements.txt
   venv/bin/python3.6 manage.py makemigrations
   venv/bin/python3.6 manage.py migrate
