33eb264647ad84537ccf5e38fcc26ab013f6f645
Darshan <darshanvkalola@gmail.com>
2020-06-11 21:27:40 -0700

    Add sudo for npm install

--- cosmos_django/jenkins_scripts/deploy.sh
+++ cosmos_django/jenkins_scripts/deploy.sh
@@ -4,7 +4,7 @@ ssh cosmos_deploy_user@167.99.154.182 <<EOF
   cd cosmos/cosmos_django
   source venv/bin/activate
   git pull
-  npm install
+  echo "Qd7#f_eMYL6ZyTU" | sudo npm install
   npm run build
   pip3 install -r requirements.txt
   venv/bin/python3.6 manage.py makemigrations
