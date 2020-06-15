c78ca66cd464cf895d857e44c24a836f8ca5a757
Darshan <darshanvkalola@gmail.com>
2020-06-14 20:41:25 -0700

    Add LiveServer tests

--- cosmos_django/cosmos_django/project_test.py
+++ cosmos_django/cosmos_django/project_test.py
@@ -1,9 +1,11 @@
 import subprocess
 
-from django.test import TestCase
+from django import test
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
 
-class TestProject(TestCase):
+class TestProject(test.TestCase):
     def test_type_checking(self):
         pr = subprocess.Popen('mypy .'.split(),
                               stdout=subprocess.PIPE,
@@ -14,3 +16,21 @@ class TestProject(TestCase):
         if pr.returncode != 0:
             print('\n\n{}\n\n'.format(out.decode("utf-8")))
             self.fail('Type checking errors.')
+
+
+class LiveServerTests(test.LiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super(LiveServerTests, cls).setUpClass()
+        cls.selenium = webdriver.Chrome(ChromeDriverManager().install())
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super(LiveServerTests, cls).tearDownClass()
+
+    def test_landing_page(self):
+        self.selenium.get(self.live_server_url)
+
+    def test_app_homepage(self):
+        self.selenium.get('{}{}'.format(self.live_server_url, '/app/'))
