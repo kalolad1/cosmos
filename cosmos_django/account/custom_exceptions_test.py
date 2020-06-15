b70cddfe731c266faf3d306b8dfaf407ad30aff0
Darshan <darshanvkalola@gmail.com>
2020-06-14 18:17:36 -0700

    Add tests for custom_exceptions.py

---
+++ cosmos_django/account/custom_exceptions_test.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+
+from . import custom_exceptions
+
+
+class TestCustomExceptions(TestCase):
+    def test_get_response_format(self):
+        exception = custom_exceptions.AccountAlreadyExistsException(
+            'Account already exists message.')
+
+        response_format = exception.get_response_format()
+
+        expected_response_format = {
+            'message': 'Account already exists message.',
+            'user_facing_message':
+            'An account already exists with that email.',
+        }
+        self.assertDictEqual(response_format, expected_response_format)
