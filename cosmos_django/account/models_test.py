aa4a6a59e7cb8f7d0eca9c5880e51af99b158656
Darshan <darshanvkalola@gmail.com>
2020-06-15 20:06:56 -0700

    Add tests to models.py

--- cosmos_django/account/models_test.py
+++ cosmos_django/account/models_test.py
@@ -1,10 +1,23 @@
+import datetime
+
 from django.test import TestCase
+import freezegun
 
 from . import custom_exceptions
 from . import models
 
 
 class TestModels(TestCase):
+    def setUp(self) -> None:
+        self.account: models.Account = models.Account.objects.create_user(
+            email='test@gmail.com', password='1234')
+        self.patient_profile: models.PatientProfile = models.PatientProfile.objects.create(
+            account=self.account,
+            first_name='John',
+            last_name='Doe',
+            date_of_birth=datetime.date(year=2000, month=2, day=10),
+            sex='male')
+
     def test_create_user_no_email_raises_value_error(self):
         self.assertRaises(custom_exceptions.DataForNewAccountNotProvided,
                           models.Account.objects.create_user,
@@ -18,11 +31,8 @@ class TestModels(TestCase):
                           password='')
 
     def test_create_user_succeeds(self):
-        user: models.Account = models.Account.objects.create_user(
-            email='test@gmail.com', password='1234')
-
-        self.assertEqual(user.email, 'test@gmail.com')
-        self.assertTrue(user.check_password(raw_password='1234'))
+        self.assertEqual(self.account.email, 'test@gmail.com')
+        self.assertTrue(self.account.check_password(raw_password='1234'))
         try:
             models.Account.objects.get(email='test@gmail.com')
         except models.Account.DoesNotExist:
@@ -30,87 +40,89 @@ class TestModels(TestCase):
 
     def test_create_superuser(self):
         superuser: models.Account = models.Account.objects.create_superuser(
-            email='test@gmail.com', password='1234')
-        self.assertEqual(superuser.email, 'test@gmail.com')
+            email='superuser@gmail.com', password='1234')
+        self.assertEqual(superuser.email, 'superuser@gmail.com')
         self.assertTrue(superuser.check_password(raw_password='1234'))
         self.assertTrue(superuser.is_admin)
         self.assertTrue(superuser.is_superuser)
         try:
-            models.Account.objects.get(email='test@gmail.com')
+            models.Account.objects.get(email='superuser@gmail.com')
         except models.Account.DoesNotExist:
             self.fail("Account not saved to db.")
 
     def test_is_staff_returns_false(self):
-        user: models.Account = models.Account.objects.create_user(
-            email='test@gmail.com', password='1234')
-        self.assertFalse(user.is_staff())
+        self.assertFalse(self.account.is_staff())
 
     def test_is_staff_returns_true(self):
-        user: models.Account = models.Account.objects.create_user(
-            email='test@gmail.com', password='1234')
-        user.is_admin = True
-        self.assertTrue(user.is_staff())
+        self.account.is_admin = True
+        self.assertTrue(self.account.is_staff())
+
+    def test_has_perm_returns_false(self):
+        self.assertFalse(self.account.has_perm('test'))
+
+    def test_has_perm_returns_true(self):
+        self.account.is_admin = True
+        self.assertTrue(self.account.has_perm('test'))
+
+    def test_has_module_perms_returns_false(self):
+        self.assertFalse(self.account.has_module_perms('app_label'))
+
+    def test_has_module_perms_returns_true(self):
+        self.account.is_admin = True
+        self.assertTrue(self.account.has_module_perms('app_label'))
 
     def test___str__(self):
-        user: models.Account = models.Account.objects.create_user(
-            email='test@gmail.com', password='1234')
-        self.assertEqual(user.__str__(), 'test@gmail.com')
+        self.assertEqual(self.account.__str__(), 'test@gmail.com')
 
     def test_username_field(self):
         self.assertEqual(models.Account.USERNAME_FIELD, 'email')
 
     def test_create_patient_profile(self):
-        account: models.Account = models.Account.objects.create_user(
-            email='test@gmail.com', password='1234')
-
-        patient_profile: models.PatientProfile = models.PatientProfile.objects.create(
-            account=account, first_name='John', last_name='Doe')
-
-        self.assertEqual(patient_profile.account, account)
-        self.assertEqual(patient_profile.first_name, 'John')
-        self.assertEqual(patient_profile.last_name, 'Doe')
+        self.assertEqual(self.patient_profile.account, self.account)
+        self.assertEqual(self.patient_profile.first_name, 'John')
+        self.assertEqual(self.patient_profile.last_name, 'Doe')
 
     def test_get_full_name(self):
-        account: models.Account = models.Account.objects.create_user(
-            email='test@gmail.com', password='1234')
+        self.assertEqual(self.patient_profile.get_full_name(), 'John Doe')
 
-        patient_profile: models.PatientProfile = models.PatientProfile.objects.create(
-            account=account, first_name='John', last_name='Doe')
+    @freezegun.freeze_time("2020-02-10")
+    def test_get_age(self):
+        self.assertEqual(self.patient_profile.get_age(), 20)
 
-        self.assertEqual(patient_profile.get_full_name(), 'John Doe')
+    @freezegun.freeze_time("2020-01-01")
+    def test_get_age_no_birthday_in_year_yet(self):
+        self.assertEqual(self.patient_profile.get_age(), 19)
+
+    @freezegun.freeze_time("2020-02-07")
+    def test_get_age_birthday_in_this_month(self):
+        self.assertEqual(self.patient_profile.get_age(), 19)
 
     def test_patient_profile__str__(self):
-        account: models.Account = models.Account.objects.create_user(
-            email='test@gmail.com', password='1234')
-
-        patient_profile: models.PatientProfile = models.PatientProfile.objects.create(
-            account=account, first_name='John', last_name='Doe')
-
-        self.assertEqual(patient_profile.__str__(), 'John Doe')
+        self.assertEqual(self.patient_profile.__str__(), 'John Doe')
 
     def test_create_visit(self):
-        account: models.Account = models.Account.objects.create_user(
-            email='test@gmail.com', password='1234')
-
-        patient_profile: models.PatientProfile = models.PatientProfile.objects.create(
-            account=account, first_name='John', last_name='Doe')
         visit: models.Visit = models.Visit.objects.create(
-            patient_profile=patient_profile,
+            patient_profile=self.patient_profile,
             visit_type=models.Visit.PHYSICAL,
             note='This is my note.')
 
-        self.assertEqual(visit.patient_profile, patient_profile)
+        self.assertEqual(visit.patient_profile, self.patient_profile)
         self.assertEqual(visit.visit_type, 'physical')
         self.assertEqual(visit.note, 'This is my note.')
 
-    def test_encounter__str__(self):
-        account: models.Account = models.Account.objects.create_user(
-            email='test@gmail.com', password='1234')
-
-        patient_profile: models.PatientProfile = models.PatientProfile.objects.create(
-            account=account, first_name='John', last_name='Doe')
+    def test_visit__str__(self):
         visit: models.Visit = models.Visit.objects.create(
-            patient_profile=patient_profile,
+            patient_profile=self.patient_profile,
             visit_type=models.Visit.PHYSICAL,
             note='This is my note.')
         self.assertEqual(visit.__str__(), 'This is my note.')
+
+    def test_medication__str__(self):
+        medication: models.Medication = models.Medication.objects.create(
+            patient_profile=self.patient_profile, name='Advil')
+        self.assertEqual(medication.__str__(), 'Advil')
+
+    def test_vaccination__str__(self):
+        vaccination: models.Vaccination = models.Vaccination.objects.create(
+            patient_profile=self.patient_profile, name='COVID-19')
+        self.assertEqual(vaccination.__str__(), 'COVID-19')
