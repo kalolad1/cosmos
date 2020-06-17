89e94395297c81fff96e6e3ed68b4f6d819e7dda
Darshan <darshanvkalola@gmail.com>
2020-06-16 18:16:44 -0700

    Remove npm run build from deploy script

--- cosmos_django/jenkins_scripts/test.sh
+++ cosmos_django/jenkins_scripts/test.sh
@@ -5,7 +5,7 @@ ssh jenkins@161.35.102.77 <<EOF
   source venv/bin/activate
   git pull
   echo "%*rmLDpi9cs&T3e" | sudo -S npm install
-  npm run build
+#  npm run build
   pip3 install -r requirements.txt
   venv/bin/python3.6 manage.py makemigrations
   venv/bin/python3.6 manage.py migrate
