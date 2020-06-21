87ca642e25e152423a3adebddf1a9d2936e852c0
Darshan <darshanvkalola@gmail.com>
2020-06-20 22:31:17 -0700

    Update deploy scripts

--- cosmos_django/deploy_scripts/test.sh
+++ cosmos_django/deploy_scripts/test.sh
@@ -3,6 +3,7 @@
 ssh jenkins@161.35.102.77 <<EOF
   cd cosmos_cloned/cosmos_django
   source venv/bin/activate
+  git reset --hard
   git pull
   echo "%*rmLDpi9cs&T3e" | sudo -S npm ci
   npm run build
