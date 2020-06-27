0de6418c584ebed7214d0894f334821eacdc1293
Darshan <darshanvkalola@gmail.com>
2020-06-27 12:25:29 -0700

    .gitignore fix

--- cosmos_django/frontend/src/constants/token_constants.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-/* Contains auth token constants. */
-Object.defineProperty(exports, '__esModule', { value: true });
-exports.REFRESH_TOKEN = exports.ACCESS_TOKEN = void 0;
-exports.ACCESS_TOKEN = 'accessToken';
-exports.REFRESH_TOKEN = 'refreshToken';
-//# sourceMappingURL=token_constants.js.map
