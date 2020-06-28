b99bfecc8532b245f34e19bf017563cde3b40541
Darshan <darshanvkalola@gmail.com>
2020-06-28 10:35:10 -0700

    Remove old encounter code

--- cosmos_django/frontend/src/components/patient/Timeline.tsx
+++ cosmos_django/frontend/src/components/patient/Timeline.tsx
@@ -3,7 +3,6 @@ import * as React from 'react';
 
 import * as types from '../../types/types';
 
-import Encounter from './Encounter';
 
 interface TimelineProps {
     encounters: Array<types.Encounter>;
@@ -11,16 +10,7 @@ interface TimelineProps {
 
 class Timeline extends React.Component<TimelineProps, any> {
     render() {
-        const encounters = this.props.encounters.map(function (encounter) {
-            return (
-                <Encounter
-                    encounterType={encounter.encounterType}
-                    note={encounter.note}
-                    key={encounter.id}
-                />
-            );
-        });
-        return <div>{encounters}</div>;
+        return <div>Timeline</div>;
     }
 }
 
