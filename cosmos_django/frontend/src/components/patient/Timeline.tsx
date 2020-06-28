2764ac97e3dd607a080d0b9ba5ba719a81822ec6
Darshan <darshanvkalola@gmail.com>
2020-06-28 16:15:48 -0700

    Add timeline skeleton

--- cosmos_django/frontend/src/components/patient/Timeline.tsx
+++ cosmos_django/frontend/src/components/patient/Timeline.tsx
@@ -1,7 +1,17 @@
-/* Contains the timeline for patient visits. */
+/* Contains the timeline for patient encounters. */
 import * as React from 'react';
 
 import * as types from '../../types/types';
+
+import NotesIcon from '@material-ui/icons/Notes';
+import TimelineDot from '@material-ui/lab/TimelineDot';
+import TimelineConnector from '@material-ui/lab/TimelineConnector';
+import TimelineContent from '@material-ui/lab/TimelineContent';
+import TimelineItem from '@material-ui/lab/TimelineItem';
+import { Timeline as TimelineComp } from '@material-ui/lab';
+import TimelineOppositeContent from '@material-ui/lab/TimelineOppositeContent';
+import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
+import Typography from '@material-ui/core/Typography';
 
 import EncounterPanel from './EncounterPanel';
 
@@ -10,25 +20,51 @@ interface TimelineProps {
 }
 
 class Timeline extends React.Component<TimelineProps, any> {
-    render() {
-        const self = this;
-        const encounterPanels = this.props.user.patientProfile.encounters.map(
-            function (encounter) {
-                return (
-                    <EncounterPanel
-                        key={encounter.id}
-                        encounter={encounter}
-                        profilePicture={
-                            self.props.user.patientProfile.profilePicture
-                        }
-                        firstName={self.props.user.patientProfile.firstName}
-                        lastName={self.props.user.patientProfile.lastName}
-                    />
-                );
-            }
-        );
+    constructor(props) {
+        super(props);
+        this.getTimelineItems = this.getTimelineItems.bind(this);
+    }
 
-        return <div>{encounterPanels}</div>;
+    getTimelineItems() {
+        const self = this;
+        return this.props.user.patientProfile.encounters.map(function (
+            encounter
+        ) {
+            return (
+                <TimelineItem key={encounter.id}>
+                    <TimelineOppositeContent>
+                        <Typography variant="body2" color="textSecondary">
+                            9:30 am
+                        </Typography>
+                    </TimelineOppositeContent>
+                    <TimelineSeparator>
+                        <TimelineDot>
+                            <NotesIcon />
+                        </TimelineDot>
+                        <TimelineConnector />
+                    </TimelineSeparator>
+                    <TimelineContent>
+                        <EncounterPanel
+                            key={encounter.id}
+                            encounter={encounter}
+                            profilePicture={
+                                self.props.user.patientProfile.profilePicture
+                            }
+                            firstName={self.props.user.patientProfile.firstName}
+                            lastName={self.props.user.patientProfile.lastName}
+                        />
+                    </TimelineContent>
+                </TimelineItem>
+            );
+        });
+    }
+
+    render() {
+        return (
+            <TimelineComp align="alternate">
+                {this.getTimelineItems()}
+            </TimelineComp>
+        );
     }
 }
 
