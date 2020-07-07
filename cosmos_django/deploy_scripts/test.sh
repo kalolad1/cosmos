760dfd3c72641d14416921fa1425b33ef5497234
Darshan <darshanvkalola@gmail.com>
2020-07-07 08:47:20 -0700

    Debug test script

--- cosmos_django/deploy_scripts/test.sh
+++ cosmos_django/deploy_scripts/test.sh
@@ -11,16 +11,16 @@ ssh jenkins@161.35.102.77 <<EOF
   }
   set -e
   trap alertFailure ERR
-  cd cosmos_cloned/cosmos_django
-  source venv/bin/activate
-  git pull
-  echo "%*rmLDpi9cs&T3e" | sudo -S npm ci
-  npm run build
-  pip3 install -r requirements.txt
-  venv/bin/python3.6 manage.py makemigrations
-  venv/bin/python3.6 manage.py migrate
-  venv/bin/python3.6 manage.py collectstatic --noinput
-  venv/bin/python3.6 manage.py test -p *_test.py
+  cd cosmos_cloned/cosmos_django; echo $?
+  source venv/bin/activate; echo $?
+  git pull; echo $?
+  echo "%*rmLDpi9cs&T3e" | sudo -S npm ci; echo $?
+  npm run build; echo $?
+  pip3 install -r requirements.txt; echo $?
+  venv/bin/python3.6 manage.py makemigrations; echo $?
+  venv/bin/python3.6 manage.py migrate; echo $?
+  venv/bin/python3.6 manage.py collectstatic --noinput; echo $?
+  venv/bin/python3.6 manage.py test -p *_test.py; echo $?
   exit 0
 EOF
 exit 0
